39e7d2fb4d5872beeca438dd72507ca4984c4fd2
A.Macedo <mariaclaramacedosouza71@gmail.com>
2026-04-08 17:12:26 -0300

    API frutas fs e json file

--- 08-04-2026/atividade/src/app.js
+++ 08-04-2026/atividade/src/app.js
@@ -1,5 +1,7 @@
 import fs from "fs/promises";
 
+import fs from "fs/promises";
+
 async function readFruits() {
   const data = await fs.readFile("./fruits.json", "utf-8");
   const fruits = JSON.parse(data);
